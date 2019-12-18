049cc37579db5fe8a83c8983ca09601e034639cc
bconrado <b_conrado@terra.com.br>
2019-12-18 16:21:17 -0300

    voice

--- Client/wwwroot/js/call.js
+++ Client/wwwroot/js/call.js
@@ -1,41 +1,32 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:44396/CallHub").configureLogging(signalR.LogLevel.Information)
-    .build();
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:44396/CallHub").withAutomaticReconnect().configureLogging(signalR.LogLevel.Information).build();
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    document.getElementById("pacient").innerHTML = user;
+    document.getElementById("place").innerHTML = msg;
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = pacientes[index];//teste
+    var message = lugar[index];//teste
+    index++;//teste
+    connection.invoke("Call", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
 connection.start().then(function () {
-    console.log('Connected!');
+    document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-connection.on("ReceiveMessage", (user, message) => {
-    const encodedMsg = user + " says " + message;
-    const li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-});
-
-document.getElementById("sendButton").addEventListener("click", event => {
-    const user = document.getElementById("userInput").value;
-    const message = document.getElementById("messageInput").value;
-    connection.invoke("Call", user, message).catch(err => console.error(err.toString()));
-    event.preventDefault();
-});
-
-async function start() {
-    try {
-        await connection.start();
-        console.log("connected");
-    } catch (err) {
-        console.log(err);
-        setTimeout(() => start(), 5000);
-    }
-};
-
-connection.onclose(async () => {
-    await start();
-});
+//testes 
+var index = 0;
+var pacientes = ["conrado0", "conrado1", "conrado2", "conrado3", "conrado4", "conrado5", "conrado6", "conrado7"];
+var lugar = ["Sala0", "Sala1", "Sala2", "Sala3", "Sala4", "Sala5", "Sala6", "Sala7"];
--- Server-Client/wwwroot/js/call.js
+++ Server-Client/wwwroot/js/call.js
@@ -7,8 +7,10 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var text = user +" "+  msg;
     document.getElementById("pacient").innerHTML  = user;
-    document.getElementById("place").innerHTML  = msg;
+    document.getElementById("place").innerHTML = msg;
+    say(text);
 });
 
 connection.start().then(function () {
@@ -27,6 +29,19 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     event.preventDefault();
 });
 var index = 0;
-var pacientes = ["conrado0", "conrado1", "conrado2", "conrado3", "conrado4", "conrado5", "conrado6", "conrado7"];
-var lugar = ["Sala0", "Sala1", "Sala2", "Sala3", "Sala4", "Sala5", "Sala6", "Sala7"];
+var pacientes = ["Bruno", "conrado1", "conrado2", "conrado3", "conrado4", "conrado5", "conrado6", "conrado7"];
+var lugar = ["Sala da Emergencia", "Sala1", "Sala2", "Sala3", "Sala4", "Sala5", "Sala6", "Sala7"];
 
+function say(m) {
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[10];
+    msg.voiceURI = "Google português do Brasil";
+    msg.volume = 1;
+    msg.rate = 1;
+    msg.pitch = 0.8;
+    msg.text = m;
+    msg.lang = "pt-BR";
+    msg.localService = true;
+    speechSynthesis.speak(msg);
+}
